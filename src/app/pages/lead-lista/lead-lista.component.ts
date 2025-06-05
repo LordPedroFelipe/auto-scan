87efcb7d5d86116d1dd72971747102d614f053da
LordPedroFelipe <pf.souza15@gmail.com>
2025-06-05 19:10:45 -0300

    feat: adjustments on pages and components

--- src/app/pages/lead-lista/lead-lista.component.ts
+++ src/app/pages/lead-lista/lead-lista.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LeadService } from 'src/app/services/lead.service';
+import { LeadModel } from 'src/app/models/lead.model';
 
 @Component({
   selector: 'app-lead-lista',
   templateUrl: './lead-lista.component.html',
   styleUrls: ['./lead-lista.component.scss']
 })
-export class LeadListaComponent {
+export class LeadListaComponent implements OnInit {
+  leads: LeadModel[] = [];
 
+  constructor(
+    private leadService: LeadService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.carregarLeads();
+  }
+
+  carregarLeads(): void {
+    this.leadService.listar().subscribe({
+      next: (res) => this.leads = res,
+      error: (err) => console.error('Erro ao carregar leads', err)
+    });
+  }
+
+  excluir(id: string): void {
+    if (confirm('Deseja realmente excluir este lead?')) {
+      this.leadService.excluir(id).subscribe(() => this.carregarLeads());
+    }
+  }
 }
