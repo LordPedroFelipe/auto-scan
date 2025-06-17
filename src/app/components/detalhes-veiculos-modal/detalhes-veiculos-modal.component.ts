1263d833a7e6bb0d5b62d335813e01410e3aabba
LordPedroFelipe <pf.souza15@gmail.com>
2025-06-17 20:08:32 -0300

    feat: adjustments on pages and components

--- src/app/components/detalhes-veiculos-modal/detalhes-veiculos-modal.component.ts
+++ src/app/components/detalhes-veiculos-modal/detalhes-veiculos-modal.component.ts
@@ -7,6 +7,28 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./detalhes-veiculos-modal.component.scss']
 })
 export class DetalhesVeiculosModalComponent {
+  
+  fotoSelecionadaIndex: number | null = null;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  abrirFotoFullscreen(index: number) {
+    this.fotoSelecionadaIndex = index;
+  }
+
+  fecharFullscreen() {
+    this.fotoSelecionadaIndex = null;
+  }
+
+  proximaFoto() {
+    if (this.fotoSelecionadaIndex !== null && this.fotoSelecionadaIndex < this.data.photoUrls.length - 1) {
+      this.fotoSelecionadaIndex++;
+    }
+  }
+
+  fotoAnterior() {
+    if (this.fotoSelecionadaIndex !== null && this.fotoSelecionadaIndex > 0) {
+      this.fotoSelecionadaIndex--;
+    }
+  }
 }
