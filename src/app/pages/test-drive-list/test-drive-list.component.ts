2f1810a51b26248e6a01f4e9bdfe0544cbc4e064
LordPedroFelipe <pf.souza15@gmail.com>
2025-07-01 20:33:05 -0300

    feat: adjustments on pages and components

--- src/app/pages/test-drive-list/test-drive-list.component.ts
+++ src/app/pages/test-drive-list/test-drive-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
 import { TestDrive } from 'src/app/models/test-drive.model';
 import { TestDriveService } from 'src/app/services/test-drive.service';
 
@@ -8,72 +9,86 @@ import { TestDriveService } from 'src/app/services/test-drive.service';
   templateUrl: './test-drive-list.component.html',
   styleUrls: ['./test-drive-list.component.scss']
 })
-export class TestDriveListComponent {
-    filtrosForm: FormGroup;
-    testDrives: TestDrive[] = [];
-    totalCount = 0;
-    pageSize = 10;
-    currentPage = 1;
+export class TestDriveListComponent implements OnInit {
+  filtrosForm: FormGroup;
+  testDrives: TestDrive[] = [];
+  totalCount = 0;
+  pageSize = 10;
+  pageIndex = 0;
+  carregando = false;
+  modoVisualizacao: 'cards' | 'tabela' = 'cards';
 
-    constructor(private fb: FormBuilder, private service: TestDriveService) {
-      this.filtrosForm = this.fb.group({
-        customerName: [''],
-        customerEmail: [''],
-        customerPhone: [''],
-        vehicleBrand: [''],
-        vehicleModel: [''],
-        // isCancelled: [''],
-        // isCompleted: [''],
-        // isActive: [''],
-        // preferredDateStart: [''],
-        // preferredDateEnd: ['']
-      });
-    }
+  displayedColumns: string[] = ['customerName', 'vehicleModel', 'preferredDate', 'acoes'];
 
-    ngOnInit(): void {
-      this.buscar();
-    }
+  constructor(
+    private fb: FormBuilder,
+    private service: TestDriveService
+  ) {
+    this.filtrosForm = this.fb.group({
+      customerName: [''],
+      customerEmail: [''],
+      customerPhone: [''],
+      vehicleBrand: [''],
+      vehicleModel: ['']
+    });
+  }
 
-    buscar(page: number = 1): void {
-      const filtros = this.filtrosForm.value;
-      const params = {
-        ...filtros,
-        pageNumber: page,
-        pageSize: this.pageSize
-      };
+  ngOnInit(): void {
+    this.buscar();
+  }
 
-      this.service.listar(params).subscribe(res => {
+  buscar(page: number = 1): void {
+    const filtros = this.filtrosForm.value;
+    const params = {
+      ...filtros,
+      pageNumber: page,
+      pageSize: this.pageSize
+    };
+
+    this.carregando = true;
+    this.service.listar(params).subscribe({
+      next: (res) => {
         this.testDrives = res.items;
         this.totalCount = res.totalCount;
-        this.currentPage = res.pageNumber;
-      });
-    }
-
-    resetar(): void {
-      this.filtrosForm.reset();
-      this.buscar(1);
-    }
-
-    onPageChange(page: number): void {
-      this.buscar(page);
-    }
-    
-    editar(loja: any): void {
-      /*const dialogRef = this.dialog.open(LojaFormComponent, {
-        width: '400px',
-        data: loja
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        if (result) {
-          this.lojaService.atualizar(loja.id, result).subscribe(() => this.carregarLojas());
-        }
-      });*/
-    }
-  
-    excluir(id: string): void {
-      if (confirm('Deseja realmente excluir esta loja?')) {
-        // this.lojaService.excluir(id).subscribe(() => this.carregarLojas());
+        this.pageIndex = res.pageNumber - 1;
+        this.carregando = false;
+      },
+      error: () => {
+        this.carregando = false;
       }
+    });
+  }
+
+  resetar(): void {
+    this.filtrosForm.reset();
+    this.buscar(1);
+  }
+
+  mudarPagina(event: PageEvent): void {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.buscar(this.pageIndex + 1);
+  }
+
+  alternarModo(): void {
+    this.modoVisualizacao = this.modoVisualizacao === 'tabela' ? 'cards' : 'tabela';
+  }
+
+  abrirCadastro(): void {
+    // lógica de edição futura
+  }
+
+  abrirDetalhes(testDrive: TestDrive): void {
+    // lógica de edição futura
+  }
+
+  editar(testDrive: TestDrive): void {
+    // lógica de edição futura
+  }
+
+  excluir(id: string): void {
+    if (confirm('Deseja realmente excluir este Test Drive?')) {
+      // lógica de exclusão futura
     }
+  }
 }
