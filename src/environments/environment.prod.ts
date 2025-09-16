133f58d415ce029d9775fafa3445d7850645b8c2
pedro.souza <pedro.souza@martinellitech.com.br>
2025-09-16 17:23:47 -0300

    faet: alteracao url server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://3.16.151.20/api'
+  // apiUrl: 'https://3.16.151.20/api'
+  apiUrl: 'https://ec2-3-16-151-20.us-east-2.compute.amazonaws.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   // apiUrl: 'https://scandrive.duckdns.org:8443/api',
   // apiUrl: 'http://ec2-3-16-151-20.us-east-2.compute.amazonaws.com/api'
-  apiUrl: 'https://3.16.151.20/api'
+  // apiUrl: 'https://3.16.151.20/api'
+  apiUrl: 'https://ec2-3-16-151-20.us-east-2.compute.amazonaws.com/api'
 };
