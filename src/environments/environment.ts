41b7f7ddf0512f5ec6e69cace73d3dabce8f5d0c
pedro.souza <pedro.souza@martinellitech.com.br>
2025-09-20 11:48:47 -0300

    feat: alteracao url server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   // apiUrl: 'https://scandrive.duckdns.org:8443/api',
   // apiUrl: 'http://ec2-3-16-151-20.us-east-2.compute.amazonaws.com/api'
   // apiUrl: 'https://3.16.151.20/api'
-  apiUrl: 'https://ec2-3-16-151-20.us-east-2.compute.amazonaws.com/api'
+  // https://ec2-3-16-151-20.us-east-2.compute.amazonaws.com/api
+  apiUrl: 'https://api.scandrive.com.br/api'
 };
